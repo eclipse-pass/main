3db2ed6c5c52c0a8b14801f6584ce29dc6b07e6d
Andrew Forward <andrew.forward@eclipse-foundation.org>
2022-07-12 10:43:01 -0300

    Ensure that docker gets attached to githubrunner group if available

--- tools/github_runner/install.sh
+++ tools/github_runner/install.sh
@@ -16,6 +16,7 @@ function install_githubrunner()
     rm -rf /opt/githubrunner && \
     mkdir /opt/githubrunner && \
     (useradd githubrunner || true) && \
+    (usermod -aG docker githubrunner || true) && \
     (cd /opt/githubrunner && \
      curl -o actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${GITHUB_RUNNER_VERSION}/actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz && \
      echo "${GITHUB_RUNNER_CHECKSUM}  actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz" | shasum -a 256 -c && \
