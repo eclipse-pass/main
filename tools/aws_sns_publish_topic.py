7afcc6a9a897cb0c42bdd1970f9dcd5621bda9d6
cshannon2 <cshann14@jh.edu>
2023-05-11 12:14:56 -0400

    Adding Region Variable to AWS deployment

--- tools/aws_sns_publish_topic.py
+++ tools/aws_sns_publish_topic.py
@@ -6,10 +6,12 @@ ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
 SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 TOPIC_ARN = os.getenv('AWS_TOPIC_ARN')
 DEPLOYMENT_ENVIRONMENT = os.getenv('DEPLOYMENT_ENVIRONMENT')
+REGION=os.getenv('AWS_REGION')
 
 MESSAGE = {"action": "DeployStart", "commitRef": "main", "deployEnv": DEPLOYMENT_ENVIRONMENT}
 
 client = boto3.client('sns',
+    region_name=REGION,
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY
     )
